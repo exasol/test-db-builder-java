52efc6445742e32ec9cf3d232203a16562b13bfc
Jakob Braun <jakob.braun@posteo.de>
2021-06-10 04:23:29 -0300

    fixed code smell

--- src/test/java/com/exasol/dbbuilder/dialects/AbstractImmediateDatabaseObjectWriterTest.java
+++ src/test/java/com/exasol/dbbuilder/dialects/AbstractImmediateDatabaseObjectWriterTest.java
@@ -13,9 +13,12 @@ import java.sql.SQLException;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 @SuppressWarnings("java:S5786") // this class should be public as implementation classes are in different packages
+@ExtendWith(MockitoExtension.class)
 public abstract class AbstractImmediateDatabaseObjectWriterTest {
     @Mock
     protected Connection connectionMock;
