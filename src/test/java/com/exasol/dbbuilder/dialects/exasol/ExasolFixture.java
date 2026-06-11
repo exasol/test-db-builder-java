59a2aed405257f84e3ccf616d7e2bcee145bac93
Christoph Pirkl <christoph.pirkl@exasol.com>
2026-06-11 14:07:18 +0200

    Upgrade Exasol version

--- src/test/java/com/exasol/dbbuilder/dialects/exasol/ExasolFixture.java
+++ src/test/java/com/exasol/dbbuilder/dialects/exasol/ExasolFixture.java
@@ -5,6 +5,6 @@ import com.exasol.containers.ExasolContainer;
 class ExasolFixture {
     @SuppressWarnings("resource") // Must be closed by caller
     static ExasolContainer<? extends ExasolContainer<?>> create() {
-        return new ExasolContainer<>("2025.2.1").withReuse(true);
+        return new ExasolContainer<>("2026.1.0").withReuse(true);
     }
 }
